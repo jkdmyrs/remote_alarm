7c26a83664f1b8a51aeaf5e79240faee5fc319d2
Jack DeMeyers <jackson.demeyers@live.com>
2019-04-10 23:23:17 -0400

    node rewrite

---
+++ alarm.js
@@ -0,0 +1,53 @@
+const Time = require('./time.js');
+
+class Alarm {
+
+  constructor() {
+    this.time = null;
+    this.alarmID = null;
+    this.isAlarmOn = false;
+  }
+
+  setAlarm(time) {
+    this.time = time;
+    this.alarmID = setInterval(() => {
+      if (!this.isAlarmOn) {
+        this.isAlarmOn = this.checkAlarmTime();
+      }
+      if (this.isAlarmOn) {
+        this.triggerAlarm();
+      }
+    }, 500);
+    console.log(`Alarm set for ${this.time.timeString}`);
+  }
+
+  checkAlarmTime() {
+    const curDate = new Date();
+    let hour = curDate.getHours();
+    let isAM;
+    if (hour > 12) {
+      hour = hour - 12;
+      isAM = false;
+    } else if (hour == 0) {
+      isAM = true;
+      hour = 12;
+    }
+    let minute = curDate.getMinutes();
+    let timeStr = `${hour.toString()}${minute.toString()}${isAM ? 'a' : 'p'}`;
+    let curTime = new Time(timeStr);
+    return this.time.compare(curTime);
+  }
+
+  triggerAlarm() {
+    clearInterval(this.alarmID);
+    console.log("Alarm triggered.");
+  }
+
+  stopAlarm() {
+    this.time = null;
+    this.isAlarmOn = false;
+    console.log("Alarm stopped");
+  }
+}
+
+module.exports = Alarm;
---
+++ time.js
@@ -0,0 +1,33 @@
+class Time {
+  constructor(timeStr) {
+    if (timeStr.length == 4) {
+      this.hour = Number.parseInt(timeStr.substring(0, 1));
+      this.minute = Number.parseInt(timeStr.substring(1, 3));
+      this.isAM = timeStr.substring(3) == 'a' ? true : false;
+    } else if (timeStr.length == 5) {
+      this.hour = Number.parseInt(timeStr.substring(0, 2));
+      this.minute = Number.parseInt(timeStr.substring(2, 4));
+      this.isAM = timeStr.substring(4) == 'a' ? true : false;
+    } else {
+      throw this._error(timeStr);
+    }
+
+    if (this.minute < 0 || this.minute > 59 || this.hour < 1 || this.hour > 12) {
+      throw this._error(timeStr);
+    }
+  }
+
+  _error(timeStr) {
+    return "Error - Cannot create Time. Invalid time: " + timeStr;
+  }
+
+  compare(time) {
+    return this.hour == time.hour && this.minute == time.minute && this.isAM == time.isAM;
+  }
+
+  get timeString() {
+    return `${this.hour}:${this.minute.toString().padStart(2, '0')} ${this.isAM ? "AM" : "PM"}`;
+  }
+}
+
+module.exports = Time;
